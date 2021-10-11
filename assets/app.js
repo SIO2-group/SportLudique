d005b9758b9db2c7d291d4ad7ab979b3c1931799
BONIFACE <contact@yohannboniface.dev>
2021-10-11 17:24:51 +0200

    :sparkles: Vue is working

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,7 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import Vue from 'vue/dist/vue.js';
+import App from './components/App.vue';
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-
-// start the Stimulus application
-import './bootstrap';
+new Vue({
+    el: "#app",
+    components: {App}
+})
