31d45ff1c0be3df9f59a32d154d335cb6d3e22d2
Yohann Boniface <edhyjox@gmail.com>
2021-10-12 19:28:19 +0200

    :wrench: Enable Scss Loader

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ Encore
     .enableVueLoader()
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
