c6a5fbd2ff2c53e7e31244886a798c846476e106
ReyFey <vic.betsch@gmail.com>
2021-10-21 12:02:54 +0200

    :recycle: Remake original SQL script

--- docs/diagrams/db/original_script.sql
+++ docs/diagrams/db/original_script.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS sport_ludique;
+DROP DATABASE IF EXISTS MLR;
 
-CREATE DATABASE IF NOT EXISTS sport_ludique;
-USE sport_ludique;
+CREATE DATABASE IF NOT EXISTS MLR;
+USE MLR;
 # -----------------------------------------------------------------------------
 #       TABLE : INVOICE
 # -----------------------------------------------------------------------------
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS INVOICE
    ID_STATUS INTEGER NOT NULL  ,
    PAY_DATE DATE NULL  
    , PRIMARY KEY (ID_INVOICE) 
- );
+ ) 
+ comment = "";
 
 # -----------------------------------------------------------------------------
 #       INDEX DE LA TABLE INVOICE
@@ -35,18 +36,45 @@ CREATE TABLE IF NOT EXISTS USER
    ROLES json NULL  ,
    PASSWORD VARCHAR(128) NULL  
    , PRIMARY KEY (ID_USER) 
- );
+ ) 
+ comment = "";
 
 # -----------------------------------------------------------------------------
-#       TABLE : CATEGORIE
+#       TABLE : REPORT
 # -----------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS CATEGORIE
+CREATE TABLE IF NOT EXISTS REPORT
  (
-   ID_CATEGORIE INTEGER NOT NULL  ,
+   ID_REPORT INTEGER NOT NULL  ,
+   ID_ARTICLE INTEGER NOT NULL  ,
+   ID_USER INTEGER NOT NULL  ,
+   CONTENT VARCHAR(255) NOT NULL  
+   , PRIMARY KEY (ID_REPORT) 
+ ) 
+ comment = "";
+
+# -----------------------------------------------------------------------------
+#       INDEX DE LA TABLE REPORT
+# -----------------------------------------------------------------------------
+
+
+CREATE  INDEX I_FK_REPORT_ARTICLE
+     ON REPORT (ID_ARTICLE ASC);
+
+CREATE  INDEX I_FK_REPORT_USER
+     ON REPORT (ID_USER ASC);
+
+# -----------------------------------------------------------------------------
+#       TABLE : CATEGORY
+# -----------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS CATEGORY
+ (
+   ID_CATEGORY INTEGER NOT NULL  ,
    NAME VARCHAR(128) NULL  
-   , PRIMARY KEY (ID_CATEGORIE) 
- );
+   , PRIMARY KEY (ID_CATEGORY) 
+ ) 
+ comment = "";
 
 # -----------------------------------------------------------------------------
 #       TABLE : ARTICLE
@@ -55,22 +83,19 @@ CREATE TABLE IF NOT EXISTS CATEGORIE
 CREATE TABLE IF NOT EXISTS ARTICLE
  (
    ID_ARTICLE INTEGER NOT NULL  ,
-   ID_CATEGORIE INTEGER NOT NULL  ,
    ID_BRAND INTEGER NOT NULL  ,
    NAME VARCHAR(128) NOT NULL  ,
    PRICE INTEGER NULL  ,
    QUANTITY INTEGER NULL  ,
    IS_ACTIVE BOOL NULL  
    , PRIMARY KEY (ID_ARTICLE) 
- );
+ ) 
+ comment = "";
 
 # -----------------------------------------------------------------------------
 #       INDEX DE LA TABLE ARTICLE
 # -----------------------------------------------------------------------------
 
-
-CREATE  INDEX I_FK_ARTICLE_CATEGORIE
-     ON ARTICLE (ID_CATEGORIE ASC);
 
 CREATE  INDEX I_FK_ARTICLE_BRAND
      ON ARTICLE (ID_BRAND ASC);
@@ -84,7 +109,8 @@ CREATE TABLE IF NOT EXISTS BRAND
    ID_BRAND INTEGER NOT NULL  ,
    NAME VARCHAR(128) NULL  
    , PRIMARY KEY (ID_BRAND) 
- );
+ ) 
+ comment = "";
 
 # -----------------------------------------------------------------------------
 #       TABLE : STATUS
@@ -95,20 +121,45 @@ CREATE TABLE IF NOT EXISTS STATUS
    ID_STATUS INTEGER NOT NULL  ,
    LABEL VARCHAR(128) NULL  
    , PRIMARY KEY (ID_STATUS) 
- );
+ ) 
+ comment = "";
+
+# -----------------------------------------------------------------------------
+#       TABLE : INCLUDE
+# -----------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS INCLUDE
+ (
+   ID_ARTICLE INTEGER NOT NULL  ,
+   ID_CATEGORY INTEGER NOT NULL  
+   , PRIMARY KEY (ID_ARTICLE,ID_CATEGORY) 
+ ) 
+ comment = "";
+
+# -----------------------------------------------------------------------------
+#       INDEX DE LA TABLE INCLUDE
+# -----------------------------------------------------------------------------
+
+
+CREATE  INDEX I_FK_INCLUDE_ARTICLE
+     ON INCLUDE (ID_ARTICLE ASC);
+
+CREATE  INDEX I_FK_INCLUDE_CATEGORY
+     ON INCLUDE (ID_CATEGORY ASC);
 
 # -----------------------------------------------------------------------------
 #       TABLE : ORDER
 # -----------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `ORDER`
+CREATE TABLE IF NOT EXISTS ORDER
  (
    ID_INVOICE INTEGER NOT NULL  ,
    ID_ARTICLE INTEGER NOT NULL  ,
    ID_USER INTEGER NOT NULL  ,
    STATUS VARCHAR(128) NULL  
    , PRIMARY KEY (ID_INVOICE,ID_ARTICLE,ID_USER) 
- );
+ ) 
+ comment = "";
 
 # -----------------------------------------------------------------------------
 #       INDEX DE LA TABLE ORDER
@@ -116,13 +167,13 @@ CREATE TABLE IF NOT EXISTS `ORDER`
 
 
 CREATE  INDEX I_FK_ORDER_INVOICE
-     ON `ORDER` (ID_INVOICE ASC);
+     ON ORDER (ID_INVOICE ASC);
 
 CREATE  INDEX I_FK_ORDER_ARTICLE
-     ON `ORDER` (ID_ARTICLE ASC);
+     ON ORDER (ID_ARTICLE ASC);
 
 CREATE  INDEX I_FK_ORDER_USER
-     ON `ORDER` (ID_USER ASC);
+     ON ORDER (ID_USER ASC);
 
 # -----------------------------------------------------------------------------
 #       TABLE : REVIEW
@@ -136,7 +187,8 @@ CREATE TABLE IF NOT EXISTS REVIEW
    COMMENT VARCHAR(128) NULL  ,
    POST_DATE DATETIME NULL  
    , PRIMARY KEY (ID_ARTICLE,ID_USER) 
- );
+ ) 
+ comment = "";
 
 # -----------------------------------------------------------------------------
 #       INDEX DE LA TABLE REVIEW
@@ -160,9 +212,14 @@ ALTER TABLE INVOICE
       REFERENCES STATUS (ID_STATUS) ;
 
 
-ALTER TABLE ARTICLE 
-  ADD FOREIGN KEY FK_ARTICLE_CATEGORIE (ID_CATEGORIE)
-      REFERENCES CATEGORIE (ID_CATEGORIE) ;
+ALTER TABLE REPORT 
+  ADD FOREIGN KEY FK_REPORT_ARTICLE (ID_ARTICLE)
+      REFERENCES ARTICLE (ID_ARTICLE) ;
+
+
+ALTER TABLE REPORT 
+  ADD FOREIGN KEY FK_REPORT_USER (ID_USER)
+      REFERENCES USER (ID_USER) ;
 
 
 ALTER TABLE ARTICLE 
@@ -170,17 +227,27 @@ ALTER TABLE ARTICLE
       REFERENCES BRAND (ID_BRAND) ;
 
 
-ALTER TABLE `ORDER`
+ALTER TABLE INCLUDE 
+  ADD FOREIGN KEY FK_INCLUDE_ARTICLE (ID_ARTICLE)
+      REFERENCES ARTICLE (ID_ARTICLE) ;
+
+
+ALTER TABLE INCLUDE 
+  ADD FOREIGN KEY FK_INCLUDE_CATEGORY (ID_CATEGORY)
+      REFERENCES CATEGORY (ID_CATEGORY) ;
+
+
+ALTER TABLE ORDER 
   ADD FOREIGN KEY FK_ORDER_INVOICE (ID_INVOICE)
       REFERENCES INVOICE (ID_INVOICE) ;
 
 
-ALTER TABLE `ORDER`
+ALTER TABLE ORDER 
   ADD FOREIGN KEY FK_ORDER_ARTICLE (ID_ARTICLE)
       REFERENCES ARTICLE (ID_ARTICLE) ;
 
 
-ALTER TABLE `ORDER`
+ALTER TABLE ORDER 
   ADD FOREIGN KEY FK_ORDER_USER (ID_USER)
       REFERENCES USER (ID_USER) ;
 
