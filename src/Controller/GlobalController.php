988dc11f32d8973dae5f40ffd97ee6ad4cbc2019
ReyFey <vic.betsch@gmail.com>
2022-04-07 17:48:00 +0200

    :sparkles: Display images instead of names

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\BrandRepository;
 use App\Repository\CategoryRepository;
+use App\Repository\PathRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class GlobalController extends AbstractController
 {
     #[Route('/', name: 'app_global')]
-    public function index(CategoryRepository $categoryRepository, BrandRepository $brandRepository): Response
+    public function index(CategoryRepository $categoryRepository, PathRepository $pathRepository): Response
     {
         return $this->render('global/index.html.twig', [
             'categories' => $categoryRepository->findBy([], ['id' => 'DESC'], 5),
-            'brands' => $brandRepository->findBy([], [], 5),
+            'brands' => $pathRepository->findBy([], [], 5),
         ]);
     }
 }
