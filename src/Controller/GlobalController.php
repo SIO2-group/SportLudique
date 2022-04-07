495eb9fd790719d238b08c746d0d230cb9ec7e34
ReyFey <vic.betsch@gmail.com>
2022-04-07 16:32:06 +0200

    :sparkles: Display only five elements

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BrandRepository;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class GlobalController extends AbstractController
 {
     #[Route('/', name: 'app_global')]
-    public function index(CategoryRepository $repository): Response
+    public function index(CategoryRepository $categoryRepository, BrandRepository $brandRepository): Response
     {
         return $this->render('global/index.html.twig', [
-            'categories' => $repository->findAll()
+            'categories' => $categoryRepository->findBy([], ['id' => 'DESC'], 5),
+            'brands' => $brandRepository->findBy([], [], 5),
         ]);
     }
 }
