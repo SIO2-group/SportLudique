1d97fffdad16401920e25d73594f443528c19ca9
ReyFey <vic.betsch@gmail.com>
2022-05-09 11:51:54 +0200

    :sparkles: Add orders

---
+++ src/Entity/Order.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'orders')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $user;
+
+    #[ORM\ManyToOne(targetEntity: Status::class, inversedBy: 'orders')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $status;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $pay_date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getStatus(): ?Status
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?Status $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getPayDate(): ?\DateTimeInterface
+    {
+        return $this->pay_date;
+    }
+
+    public function setPayDate(?\DateTimeInterface $pay_date): self
+    {
+        $this->pay_date = $pay_date;
+
+        return $this;
+    }
+}
