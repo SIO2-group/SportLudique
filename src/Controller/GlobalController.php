ad8fa18a8136e14e198f02ccf5303eb4475b56d7
ReyFey <vic.betsch@gmail.com>
2022-05-09 11:07:28 +0200

    :zap: Improve brand system path

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Repository\BrandRepository;
 use App\Repository\CategoryRepository;
-use App\Repository\FileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class GlobalController extends AbstractController
 {
     #[Route('/', name: 'app_global')]
-    public function index(CategoryRepository $categoryRepository, FileRepository $fileRepository): Response
+    public function index(CategoryRepository $categoryRepository, BrandRepository $brandRepository): Response
     {
         return $this->render('global/index.html.twig', [
             'categories' => $categoryRepository->findBy([], ['id' => 'DESC'], 5),
-            'files' => $fileRepository->findBy([], ['article' => 'ASC'], 5)
+            'brands' => $brandRepository->findBy([], [], 5)
         ]);
     }
 }
