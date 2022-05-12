bdb0000b05f95c35c5402f8343e064b62e799e07
ReyFey <vic.betsch@gmail.com>
2022-05-12 10:49:24 +0200

    :sparkles: Added article details page

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\Article1Type;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/article')]
+class ArticleController extends AbstractController
+{
+    #[Route('/{id}', name: 'app_article_show', methods: ['GET'])]
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+}
