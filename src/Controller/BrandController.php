f59e0355f4f0a9ef2dc9ac459fbb14dcd3e73514
ReyFey <vic.betsch@gmail.com>
2022-05-16 01:09:24 +0200

    :sparkles: Add brands pages

---
+++ src/Controller/BrandController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Brand;
+use App\Form\BrandType;
+use App\Repository\BrandRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/brand')]
+class BrandController extends AbstractController
+{
+    #[Route('/{id}', name: 'app_brand_show', methods: ['GET'])]
+    public function show(Brand $brand): Response
+    {
+        return $this->render('brand/show.html.twig', [
+            'brand' => $brand,
+            'articles' => $brand->getArticles()
+        ]);
+    }
+}
